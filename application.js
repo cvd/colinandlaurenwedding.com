b91fd14d0bbc0065118387bf4e35ca876f77052f
Colin Van Dyke <colin@everfi.com>
2011-08-21 20:22:48 -0400

    cleaning up the js a bunch

--- application.js
+++ application.js
@@ -9,79 +9,75 @@
 
     options = $.extend(defaults, options);
 
-    this.each(function(){
+    var tabs = [ "welcome", "about-us", "our-story",
+                    "ceremony-reception", "travel-info",
+                    "registry"]
 
+    function slide(element, event){
+      event.preventDefault();
+      $("#nav a").removeClass("active");
+      var currentTab = element.attr("href").replace("#","");
+      $("#nav ."+currentTab).addClass("active");
+      animateView(currentTab);
+    }
 
-      //Bind controls
-      $("#nav").delegate("a", "click", function(event){
-        element = $(this);
-        slide(element, event);
-      });
+    // Updates the view with animation
+    function animateView(currentTab){
+      var tabIndex = indexOf(tabs, currentTab),
+      windowWidth = $(window).width(),
+      slideSize = (0 - tabIndex * windowWidth);
 
-      //Functions
-      function slide(element, event){
-        event.preventDefault();
-        $("#nav a").removeClass("active");
-        currentTab = element.attr("href").replace("#","");
-        $("#nav ."+currentTab).addClass("active");
-        animate(currentTab);
+      $(".main .inner").animate({ marginLeft : slideSize },
+        defaults.slideDuration,
+        defaults.slideEasing,
+        tabSwitched(currentTab)
+      );
+    }
+
+    // Updates the view without the animation, used on pageload
+    function updateView(currentTab){
+      var tabIndex = indexOf(tabs, currentTab),
+      windowWidth = $(window).width(),
+      slideSize = (0 - tabIndex * windowWidth);
+      $(".main .inner").css({ marginLeft : slideSize });
+      tabSwitched(currentTab);
+    }
+
+    // indexOf util (because IE doesn't have Array#indexOf, thanks IE)
+    function indexOf(arr, item){
+      for(var i=0,l=arr.length;i<l;i++){
+        if(arr[i] === item) return i;
       }
+    }
 
-      function animate(currentTab){
-
-        slideSize = 0;
-
-        if(currentTab == "welcome") {
-          slideSize = "0";
-        }
-
-        if(currentTab == "about-us") {
-          slideSize = "-" + $(window).width() + "px";
-        }
-
-        if(currentTab == "our-story") {
-          slideSize = "-" + $(window).width() * 2 + "px";
-        }
-
-        if(currentTab == "ceremony-reception") {
-          slideSize = "-" + $(window).width() * 3 + "px";
-        }
-
-        if(currentTab == "travel-info") {
-          slideSize = "-" + $(window).width() * 4 + "px";
-        }
-        if(currentTab == "registry") {
-          slideSize = "-" + $(window).width() * 5 + "px";
-        }
-
-        $(".main .inner").animate({
-          marginLeft : slideSize
-        }, defaults.slideDuration, defaults.slideEasing, function(){
-          window.location.hash = "/" + currentTab;
-          $(".section").css("height","1px");
-          $("#"+currentTab).css("height","auto");
-        });
-
-
+    function tabSwitched(currentTab){
+      // return function that has currentTab in its scope
+      return function(){
+        window.location.hash = "/" + currentTab;
+        $(".section").css("height", "1px");
+        $("#" + currentTab).css("height", "auto");
       }
+    }
 
-      (function(){
-
-        //Load page to a specific section
-        if(window.location.hash) {
-          currentTab = window.location.hash.replace(/#*\//g,"");
-          animate(currentTab);
-          $("#nav a").removeClass("active");
-          $("." + currentTab).addClass("active");
-        }
-        else {
-          $(".section").css("height","1px");
-          $("#welcome").css("height","auto");
-        }
-
-      })();
-
+    //Bind controls
+    $("#nav").delegate("a", "click", function(event){
+      var element = $(this);
+      slide(element, event);
     });
+
+    (function(){
+      //Load page to a specific section
+      if(window.location.hash) {
+        var currentTab = window.location.hash.replace(/#*\//g,"");
+        updateView(currentTab);
+        $("#nav a").removeClass("active");
+        $("." + currentTab).addClass("active");
+      }
+      else {
+        $(".section").css("height","1px");
+        $("#welcome").css("height","auto");
+      }
+    })();
 
   };
 
