7532ba50546f4d84824b84924365eeaf63c352ed
Colin Van Dyke <colin@everfi.com>
2012-03-07 17:25:27 -0500

    Add spec helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,10 @@
+ENV['RACK_ENV'] = "test"
+require_relative '../lib/wedding'
+
+require 'rspec'
+require 'rack/test'
+
+RSpec.configure do |config|
+  config.mock_with :rspec
+  #config.include Rack::Test::Methods
+end
