0ef2e6fd69a5d8e499f6bb13b025a01d2cad12b1
Colin Van Dyke <colin@everfi.com>
2012-03-07 22:38:31 -0500

    Start speccing out the parse email class

--- lib/models/parse_email.rb
+++ lib/models/parse_email.rb
@@ -1,3 +1,31 @@
 class ParseEmail
 
+  def initialize(params)
+    @params = params
+  end
+
+  def filename
+    attachments["attachment1"]["filename"]
+  end
+
+  #{:filename=>"fugu.png", :type=>"image/png", :name=>"attachment1", :tempfile=>"file", :head=>"Content-Disposition: form-data; name=\"attachment1\"; filename=\"fugu.png\"\nContent-Type: image/png\n", "tempfile"=>nil}
+  def attachments
+    hash = JSON.parse(@params["attachment-info"])
+    hash.map do |file_key, values|
+      values[:file] = @params[file_key]
+      HashWithIndifferentAccess.new(values)
+    end
+  end
+
+  def attachment_keys
+    attachments.keys
+  end
+
+  def create_files
+    attachments.each do |attachment|
+      #create versions && store files
+    end
+  end
+
 end
+
--- spec/models/parse_email_spec.rb
+++ spec/models/parse_email_spec.rb
@@ -1,8 +1,24 @@
 require 'spec_helper'
+require 'tempfile'
 require_relative "../../lib/models/parse_email"
 
 describe ParseEmail do
-  it "should parse email messages"
+
+  before do
+    e = File.join(File.dirname(__FILE__), "..", "fixtures", "email.yaml")
+    @params = YAML.load(File.read(e))
+
+    img = File.join(File.dirname(__FILE__), "..", "fixtures", "fugu.png")
+    @tmpfile = Tempfile.new("email")
+    @tmpfile.write File.read(img)
+    @params['attachment1']['tempfile'] = @templfile
+  end
+
+  it "should parse email messages" do
+    email = ParseEmail.new(@params)
+    email.attachments.first[:filename].should == "fugu.png"
+  end
+
   it "should pull out each attachment"
   it "should create a new Image record for each iamge attachment"
   it "should use the subject line of the email as a title"
