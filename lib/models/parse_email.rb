299469f64a531f97c592313e437d5c3ef97a348a
Colin Van Dyke <colin@everfi.com>
2012-03-09 10:02:49 -0500

    Pull info out of the params. Integrate with the S3Uploader

--- lib/models/parse_email.rb
+++ lib/models/parse_email.rb
@@ -1,11 +1,31 @@
 class ParseEmail
 
   def initialize(params)
-    @params = params
+    @params = HashWithIndifferentAccess.new(params)
   end
 
-  def filename
-    attachments["attachment1"]["filename"]
+  def description
+    @params[:text].chomp
+  end
+
+  def title
+    @params[:subject]
+  end
+
+  def from_email
+    if /(.+)<(.+)>/ =~ @params[:from]
+      return $2.strip
+    else
+      @params[:from]
+    end
+  end
+
+  def from_name
+    if /(.+)<(.+)>/ =~ @params[:from]
+      return $1.strip
+    else
+      return @params[:from].split("@").first
+    end
   end
 
   #{:filename=>"fugu.png", :type=>"image/png", :name=>"attachment1", :tempfile=>"file", :head=>"Content-Disposition: form-data; name=\"attachment1\"; filename=\"fugu.png\"\nContent-Type: image/png\n", "tempfile"=>nil}
@@ -23,8 +43,30 @@ class ParseEmail
 
   def create_files
     attachments.each do |attachment|
-      #create versions && store files
+      metadata = {
+        :content_type => attachment[:type],
+        :cache_control => "max-age=2592000"
+      }
+      S3Uploader.store_file("colinandlauren", attachment.tempfile, file_name, metadata)
+      photo = Photo.create({
+        :title => title,
+        :description => description,
+        :image_url => s3_image.url,
+        :user_email => from_email,
+        :user_name => from_name
+      })
+
     end
+  end
+
+  protected
+
+  def file_name
+    "#{uuid}.#{File.extname(attachment[:filename])}"
+  end
+
+  def uuid
+    UUIDTools::UUID.timestamp_create
   end
 
 end
--- spec/models/parse_email_spec.rb
+++ spec/models/parse_email_spec.rb
@@ -19,6 +19,21 @@ describe ParseEmail do
     email.attachments.first[:filename].should == "fugu.png"
   end
 
+  it "should know the name of the sender" do
+    email = ParseEmail.new(@params)
+    email.from_name.should == "Colin Van Dyke"
+  end
+  it "should use the email handle if the from has no name" do
+    @params["from"] = "vandyke.colin@gmail.com"
+    email = ParseEmail.new(@params)
+    email.from_name.should == "vandyke.colin"
+  end
+
+  it "knows the email address of the sender" do
+    email = ParseEmail.new(@params)
+    email.from_email.should == "vandyke.colin@gmail.com"
+  end
+
   it "should pull out each attachment"
   it "should create a new Image record for each iamge attachment"
   it "should use the subject line of the email as a title"
